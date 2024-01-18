c0cf136cede94ed095408413447fba40e93b4daf
koushik hs <103168080+khs0108@users.noreply.github.com>
2024-01-18 22:17:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Data Submission</title>
+</head>
+<body>
+<form action="process.php" method="post">
+  <label for="data">Enter your data:</label>
+  <input type="text" id="data" name="data">
+  <button type="submit">Submit</button>
+</form>
+</body>
+</html>
